5ee1d108e460256fd2e7308b09f8cbeab83f5892
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-08-27 00:19:46 +0900

    Create array-loop.js
    add array-loop.js

---
+++ web2.nodejs/syntax/array-loop.js
@@ -0,0 +1,18 @@
+//var number = [1, 400, 12, 34];
+//var i = 0;
+//var total = 0;
+//while( i < 5  ){
+//  console.log(number.[i]);
+//  total = total + number[i];
+//  i = i + 1;
+//}
+//console.log(`total : ${total}`);
+
+var number = [1,400,12,34];
+var i = 0;
+var total = 0;
+while(i < number.length){
+  total = total + number[i];
+  i = i + 1;
+}
+console.log(`total : ${total}`);
