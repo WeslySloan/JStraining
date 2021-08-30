12d5dad1e3d11e71c2ed2a5c7f0752d9a7006e7e
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-08-31 02:55:12 +0900

    Create fuction3.js
    add fuction3.js

---
+++ web2.nodejs/syntax/fuction3.js
@@ -0,0 +1,10 @@
+//Math.round(A,B) = A.B값 반올림하는 함수
+
+console.log(Math.round(1.6)); //2 
+console.log(Math.round(1.4)); //1
+ 
+function sum(first, second){ // parameter 매개 변수 = (first, second) = 함수지정용 변수
+  console.log(first+second);
+}
+ 
+sum(2,4); // argument 인자 = (2.4) = 함수입력내용물
