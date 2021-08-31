4e34a84817225681e0366de9d6ec5e2441b0e148
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-09-01 03:21:31 +0900

    Create fuction4.js
    fuction.js add

---
+++ web2.nodejs/syntax/fuction4.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+ 
+function sum(first, second){ // parameter
+  return first+second; // return 이후에는 어떤 명령어도 받지않음. return 에서 종료되기때문
+  console.log("this is nor work");
+}
+ 
+console.log(sum(2,4)); // argument
