927d5f1f33b43f06aa3efadfcf54cf7eb3d523d9
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-09-06 21:01:16 +0900

    Update sync.js
    fix the error of sync.js

--- web2.nodejs/syntax/sync.js
+++ web2.nodejs/syntax/sync.js
@@ -1,14 +1,27 @@
-var fs = require('fs');
- 
 /*
+동기적 
+
 //readFileSync
 console.log('A');
 var result = fs.readFileSync('syntax/sample.txt', 'utf8');
-console.log(result);
+console.log(result); 
 console.log('C');
 */
  
- 
+/*
+비동기적 
+*/ 
+/*
+var fs = require('fs');
+console.log('A'); 
+fs.readFile('syntax/sample.txt', 'utf8', function(err, result){ 
+    console.log(result);
+});
+console.log('C'); 
+*/
+
+var fs = require('fs');
+
 console.log('A');
 fs.readFile('syntax/sample.txt', 'utf8', function(err, result){
     console.log(result);
